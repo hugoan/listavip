2754269990dd92b0a646ab86435548b37bf15dc0
Hugo <hugo.nobreg@gmail.com>
2017-03-01 18:07:43 -0300

    Exercícios

--- src/main/java/br/com/alura/listavip/HomeController.java
+++ src/main/java/br/com/alura/listavip/HomeController.java
@@ -4,15 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import br.com.alura.enviadorEmail.EmailService;
 import br.com.alura.listavip.model.Convidado;
-import br.com.alura.listavip.repository.ConvidadoRepository;
+import br.com.alura.listavip.service.ConvidadoService;
 
 @Controller
 public class HomeController {
 
 	@Autowired
-	private ConvidadoRepository repository;
+	ConvidadoService convidadoService;
+
+	Convidado novoConvidado = new Convidado();
 
 	@RequestMapping("/")
 	public String index() {
@@ -21,8 +25,21 @@ public class HomeController {
 
 	@RequestMapping("listaconvidados")
 	public String listaConvidados(Model model) {
-		Iterable<Convidado> convidados = repository.findAll();
+		Iterable<Convidado> convidados = convidadoService.obterTodos();
 		model.addAttribute("convidados", convidados);
+		return "listaconvidados";
+	}
+
+	@RequestMapping(value = "salvar", method = RequestMethod.POST)
+	public String salvar(Convidado novoConvidado, Model model) {
+
+		convidadoService.salvar(novoConvidado);
+
+		new EmailService().enviar(novoConvidado.getNome(), novoConvidado.getEmail());
+
+		Iterable<Convidado> convidados = convidadoService.obterTodos();
+		model.addAttribute("convidados", convidados);
+
 		return "listaconvidados";
 	}
 
